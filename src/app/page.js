f219c4b13ad9351f6f133cae892bd10ab4b0b509
hoang-ngo-dev <ngoviethoangdev@gmail.com>
2024-04-22 18:05:28 +0700

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,35 @@
 import Header from "@/components/layout/Header";
 import Hero from "@/components/layout/Hero";
+import HomeMenu from "@/components/layout/HomeMenu";
+import SectionHeaders from "@/components/layout/SectionHeaders";
 
 
 export default function Home() {
   return (
     <>
-   <Header/>
-   <Hero/>
+      <Header />
+      <Hero />
+      <HomeMenu />
+      <section className="text-center my-16">
+        <SectionHeaders subHeader={'Our story'} mainHeader={'About us'} />
+        <div className="text-gray-500 max-w-md mx-auto mt-4 flex-col gap-4">
+          <p>Lorem ipum </p>
+          <p>Lorem ipum </p>
+          <p>Lorem ipum </p>
+
+        </div>
+
+      </section>
+      <section className="text-center my-8">
+        <SectionHeaders subHeader={'Don\'t hesitate'} mainHeader={'Contact us'} />
+        <div className="mt-8">
+          <a className="text-4xl underline text-gray-500" href="tel:+46783123123">+46 783 123 123</a>
+        </div>
+
+      </section>
+      <footer className="border-t p-8 text-center text-gray-500 mt-16">
+        &copy; 2023 All rights reserved
+      </footer>
     </>
   );
 }
--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -6,15 +6,17 @@ const Hero = () => {
   return (
     <section className='grid grid-cols-2'>
         <div>
-        <h1 className='text-4xl font-semibold'>Everything is better with a Pizza
+        <h1 className='text-4xl font-semibold'>Everything <br/>
+         is better <br/>
+          with a&nbsp; <span className='text-primary'>Pizza</span> 
         </h1>
         <p className='my-4 text-gray-500'>Pizza is the missing piece that makes every day complete, a simple yet delicious joy in life </p>
         <div className='flex gap-4'>
             <button className='bg-primary uppercase flex items-center text-white px-4 py-2 rounded-full text-sm'>Order now <Right/></button>
-            <button className='flex gap-2 py-2 text-gray-600'>Learn more</button>
+            <button className='flex gap-2 py-2 text-gray-600 font-semibold'>Learn more <Right/></button>
         </div>
         </div>
-        <div className='relative'> <Image src={'/pizza.png'} layout={'fill'} objectFit={'contain'} alt={'pizza'}/></div>
+        <div className='relative'><Image src={'/pizza.png'} layout={'fill'} objectFit={'contain'} alt={'pizza'}/></div>
        
     </section>
   )
